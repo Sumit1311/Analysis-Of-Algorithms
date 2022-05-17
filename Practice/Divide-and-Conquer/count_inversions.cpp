0f0ff38ef81c7f0f197e91d44a5558905ef41318
Sumit Toshniwal <sumit_toshniwal@persistent.com>
2022-05-17 18:25:08 +0530

    Adding inversion count code

--- Practice/Divide-and-Conquer/count_inversions.cpp
+++ Practice/Divide-and-Conquer/count_inversions.cpp
@@ -8,18 +8,27 @@
 #include <algorithm>
 
 using namespace std;
+typedef int * IntPtr;
 typedef vector<int> VectorInt;
 
-void printVector(VectorInt &arr)
+void printVector(IntPtr arr, int begin, int end)
 {
+	cout<<"Vector : ";
+	for(int i = begin; i <= end; ++i) {
+		cout<<arr[i]<<",";
+	}
+	cout<<endl;
 }
 
-int approach1(VectorInt &array)
+//Wrong approach and understanding
+int approach1(IntPtr array, int begin, int end)
 {
+	/// !!!!!! DONT USE THIS !!!!!
+	/// Incorrect way
     int countInversions = 0;
-    VectorInt tempV(array);
+    VectorInt tempV(array+begin, array+end);
     sort(tempV.begin(), tempV.end());
-    for (int i = 0; i < array.size(); ++i)
+    for (int i = 0; i < tempV.size(); ++i)
     {
         if (tempV[i] != array[i])
         {
@@ -29,13 +38,13 @@ int approach1(VectorInt &array)
     return countInversions;
 }
 
-int approach2(VectorInt &array)
+int approach2(IntPtr array, int begin, int end)
 {
     int countInversions = 0;
 
-    for (int i = 0; i < array.size(); ++i)
+    for (int i = begin; i < end; ++i)
     {
-        for (int j = i + 1; j < array.size(); ++j)
+        for (int j = i + 1; j <= end; ++j)
         {
             if (array[i] > array[j])
             {
@@ -47,9 +56,81 @@ int approach2(VectorInt &array)
     return countInversions;
 }
 
+int countInversions(IntPtr array, int begin, int mid, int end) {
+	int count = 0;
+	
+	IntPtr first = new int[mid - begin + 1];
+	IntPtr second = new int[end - mid];
+//	cout<<"Before ";
+//	printVector(array, begin, end);
+	for(int i = begin; i <= mid; ++i) {
+		first[i-begin] = array[i];
+	}
+	
+	for(int i = mid + 1; i <= end; ++i) {
+		//cout<<"setting second : "<<array[i]<<endl;
+		second[i - mid - 1] = array[i];
+	}
+	
+//	cout<<begin<<"-"<<mid<<"-"<<end<<endl;
+//
+//	cout<<"First : ";
+//	printVector(first, 0, (mid - begin));
+//	
+//	cout<<"Second : ";
+//	printVector(second, 0, (end - mid) - 1);
+	
+	int i = 0, j = 0, k = begin;
+	
+	while(i <= (mid - begin) && j <= ((end - mid) -1 )) {
+		if(first[i] >= second[j]) {
+			array[k] = second[j];
+			j++;
+			count+=(mid - begin - i + 1);
+		} else {
+			array[k] = first[i];
+			i++;
+		}
+		k++;
+	}
+	
+	while(i <= (mid- begin)) {
+			array[k] = first[i];
+			i++;
+			k++;
+	}
+	
+	while( j <= (end - mid - 1)) {
+		array[k] = second[j];
+			j++;
+			k++;
+	}
+	
+	
+//	printVector(array, begin, end);
+//	cout<<count<<endl;
+	return count;
+	
+}
+
+int approach3(IntPtr array, int begin, int end)
+{   
+    if(begin >= end) {
+		return 0;
+	}
+	int mid = begin + ((end - begin) / 2);
+    //cout<<"counting for "<<begin<<"-"<<mid<<"-"<<end<<endl;
+	int count = approach3(array, begin, mid) + approach3(array, mid+1, end);
+	return count + countInversions(array, begin, mid, end);
+}
+
 int main()
 {
-    VectorInt arr{3, 2, 7, 4, 6, 8};
-    cout << approach1(arr) << endl;
-    cout << approach2(arr) << endl;
+    //int arr[] = {3, 2, 7, 4, 6, 8};
+    int arr[] = {8, 4, 2, 1};
+    //int size = 6;
+    int size = 4;
+    cout << approach1(arr, 0, 3 ) << endl;
+    cout << approach2(arr, 0, 3 ) << endl;
+    cout << approach3(arr, 0, 3 ) << endl;
 }
